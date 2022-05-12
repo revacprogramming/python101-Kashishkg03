48c3e3ed2da80fa70f0b2ccb0a7184b742840b83
Kashish03 <97089600+Kashishkg03@users.noreply.github.com>
2022-05-12 17:27:03 +0000

    program 4

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,3 +1,8 @@
-# Conditional Execution
-
-hrs = input("Enter hours? ")
+hrs = input("Enter Hours: ")
+h = float(hrs)
+rate = input("enter the rate: ")
+r=float(rate)
+if h<=40:
+	print(h*r)
+elif h>40:
+    print(40*r + (h-40)*1.5*r)
--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -1,12 +1,15 @@
-# Functions
+score = float(input("Enter Score from'0.0-1.0'"))
+if score<=1.0:
+ if score >=0.9:
+    print("A")
+elif score>= 0.8:
+    print("B")
+elif score>= 0.7:
+    print("C")
+elif score>= 0.6:
+    print("D")
+elif score< 0.6:
+    print("F")
+else:
+    print("out of order from'0.1-1.0'")
 
-
-def computepay(h, r):
-    pass  # ...
-
-
-hrs = float(input("Enter hours? "))
-rte = float(input("Enter rate per hour? "))
-
-p = computepay(hrs, rte)
-print("Pay", p)
