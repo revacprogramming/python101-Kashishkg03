dedaf6fe8371c4d4ea15e2ac8eb2df413176ce5e
Kashish03 <97089600+Kashishkg03@users.noreply.github.com>
2022-06-02 17:42:53 +0000

    program 13

--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -1,2 +1,18 @@
 # Network Programming
 # https://www.py4e.com/lessons/network
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+d =dict()
+for line in handle:
+    if not line.startswith("from"):
+        continue
+   
+        
+lst = list()
+for value,count in d.items():
+    lst.append(values,count)
+lst.sort()
+for value,count in lst:
+    print(value,count)
