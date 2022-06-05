13084fbe2105de0730004973f316558f3aeba0a9
Kashish03 <97089600+Kashishkg03@users.noreply.github.com>
2022-06-05 17:10:23 +0000

    program 13

--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -1,8 +1,6 @@
 # Network Programming
 # https://www.py4e.com/lessons/network
 name = input("Enter file:")
-if len(name) < 1:
-    name = "mbox-short.txt"
 handle = open(name)
 d =dict()
 for line in handle:
