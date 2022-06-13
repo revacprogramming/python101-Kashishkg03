5c1252ddb7db3b489526670592d2545a87b93981
Kashish03 <97089600+Kashishkg03@users.noreply.github.com>
2022-06-13 18:08:00 +0000

    program 5

--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -4,7 +4,6 @@ def computepay(h, r):
     else:
         value=40*r + (h-40)*r*1.5
     return value
-
 hrs = int(input("Enter Hours:"))
 rate= float(input("Enter rate: "))
 p = computepay(hrs, rate)
