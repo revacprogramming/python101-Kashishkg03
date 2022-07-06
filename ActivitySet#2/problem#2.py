d169f6506185727cc30fc8a2954bd628e1def93b
Kashish03 <97089600+Kashishkg03@users.noreply.github.com>
2022-07-06 16:22:28 +0000

    problem 18

--- ActivitySet#2/problem#2.py
+++ ActivitySet#2/problem#2.py
@@ -1,11 +1,9 @@
 def add(a, b):
     return(a+b)
     pass
-
 def output(a, b, sum):
     print(a,"+",b,"is",sum)
     pass
-
 def main():
     a,b =map(int,input("enter two numbers:").split())
     sum = add(a, b)
