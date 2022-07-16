dc4238219c52913c4323c0eea8edf4578bdd5bbb
Kashish03 <97089600+Kashishkg03@users.noreply.github.com>
2022-07-16 17:31:10 +0000

    program 5

--- ActivitySet#2/problem#5.py
+++ ActivitySet#2/problem#5.py
@@ -2,11 +2,12 @@
 
 def get_cs():
     """get string input"""
-
+    a = input("enter the string:")
+    return(a)
 
 def cs_to_dict(cs):
     """convert connect string to a dictionary"""
-
+gi
 
 def dict_to_cs(d):
     """convert a dictionary to connect string"""
