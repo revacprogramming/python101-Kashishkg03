1e0e64505f45d12402b8ac913f46dd6d65aaa82e
Kashish03 <97089600+Kashishkg03@users.noreply.github.com>
2022-07-20 14:08:22 +0000

    program 6

--- ActivitySet#2/problem#6.py
+++ ActivitySet#2/problem#6.py
@@ -2,10 +2,15 @@
 
 class Menu:
     """fill in class definition here"""
-
+    def add(self,food,price):
+        self.food = food
+        self.price = price
+    def show(self):
+        print(self.food,self.price)
 
 m = Menu()  # Menu is a class
 m.add("idly", 10)
-m.add("vada". 20)
+m.show()
 
+m.add("vada", 20)
 m.show()
